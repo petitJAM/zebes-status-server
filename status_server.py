ecb1ce63cefccbf7d9bf2d09a0f8276a6de4d1e0
Alex Petitjean <petitjam@rose-hulman.edu>
2014-04-26 22:13:56 -0400

    Bunch of changes I forgot to commit, not gonna figure it out now\! You probably aren't reading this anyway.

--- status_server.py
+++ status_server.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python
 
+import time
 import os.path
+import re
 import subprocess as sp
 
 import tornado.ioloop
 import tornado.httpserver
 import tornado.web
 
+def init_minecraft_with_result(command):
+	return sp.check_output(["/home/motherbrain/minecraft/mc_vanilla/init/minecraft", command])
+
 def check_status():
-	return sp.check_output(["/home/motherbrain/minecraft/mc_vanilla/init/minecraft", "status"])
+	return init_minecraft_with_result("status")
 
 GENERATE_LOCKED = False
 def regenerate_overviewer():
+	global GENERATE_LOCKED
+
 	if not GENERATE_LOCKED:
 		GENERATE_LOCKED = True
 		sp.call(["/home/motherbrain/init/minecraft", "overviewer"])
@@ -20,21 +27,38 @@ def regenerate_overviewer():
 	else:
 		return False
 
-print check_status()
+
+is_running_regexp = re.compile(r'.*(not running).*')
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
-		st = check_status()
-		self.write("<h2>Zebes status</h2><p>" + st + "</p>")
+		status = check_status()
+		running = (is_running_regexp.search(status) is None)
+
+		playercount, connected = "", ""
+
+		if running:
+			playercount = int(init_minecraft_with_result("playercount"))
+			connected   = init_minecraft_with_result("connected")
+			# remove the server log tag
+			connected   = connected[33:]
+
+			# There's a bug with ./minecraft playercount
+			if len(connected) == 1:
+				playercount = "0"
+
+		self.render("index.html", status=status, playercount=playercount, connected=connected)
 
 class OverviewerHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.redirect("static/index.html")
+		self.redirect("static/map/index.html")
 
 class UpdateOverviewerHandler(tornado.web.RequestHandler):
+	@tornado.web.asynchronous
 	def get(self):
 		self.render("generating_map.html")
-		regenerate_overviewer()
+		if not regenerate_overviewer():
+			print "Already running!"
 
 class Application(tornado.web.Application):
 	def __init__(self):
@@ -42,10 +66,10 @@ class Application(tornado.web.Application):
 			(r"/", MainHandler),
 			(r"/map", OverviewerHandler),
 			(r"/mapupdate", UpdateOverviewerHandler),
-			(r"/static", tornado.web.StaticFileHandler, { "path": "./map" }),
+			(r"/map_static", tornado.web.StaticFileHandler, { "path": "./static/map" }),
 		]
 		settings = dict(
-			static_path=os.path.join(os.path.dirname(__file__), "map"),
+			static_path=os.path.join(os.path.dirname(__file__), "static"),
 			debug=True,
 		)
 		tornado.web.Application.__init__(self, handlers, **settings)
